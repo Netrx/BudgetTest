ea65c83c3398e4849868daf88e2569cedc10bdec
Netrx <53960054+Netrx@users.noreply.github.com>
2026-08-18 03:22:08 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='budget-app-v8-20260817';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','https://fonts.googleapis.com/css2?family=Inter:opsz@14..32&display=swap'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
